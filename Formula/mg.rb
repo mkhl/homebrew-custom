79b7e7e78017f66e16a29ef03c6ee2623bffde32
Martin Kühl <martin.kuehl@gmail.com>
2016-01-09 20:54:34 +0100

    mg 20160103

--- Formula/mg.rb
+++ Formula/mg.rb
@@ -1,16 +1,13 @@
 class Mg < Formula
   desc "Small Emacs-like editor"
   homepage "http://devio.us/~bcallah/mg/"
-  url "http://devio.us/~bcallah/mg/mg-20151130.tar.gz"
-  sha256 "de589aa5b2e5067443a7c3ecf9cea4747056b2ebd6c245e78e029f1a89b62ee1"
+  url "http://devio.us/~bcallah/mg/mg-20160103.tar.gz"
+  sha256 "4abd059ba3e0d59626104a21812ae33a37ee1f6ddaafdb33511f38d21057fae6"
 
   conflicts_with "mg3a", :because => "both install `mg` binaries"
 
   def install
-    system "make"
-    bin.install "mg"
-    doc.install "tutorial"
-    man1.install "mg.1"
+    system "make", "install", "PREFIX=#{prefix}", "MANDIR=#{man}"
   end
 
   test do
