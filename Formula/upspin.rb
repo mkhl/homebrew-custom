2e327425e39d7c8a151917744e935bd36544e64e
Martin Kühl <martin.kuehl@gmail.com>
2018-07-01 20:31:48 +0200

    upspin: upspin@1e73992-augie@b93eee0

--- Formula/upspin.rb
+++ Formula/upspin.rb
@@ -2,8 +2,8 @@ class Upspin < Formula
   desc "upspin"
   homepage "https://upspin.io"
   url "https://upspin.io/dl/upspin.darwin_amd64.tar.gz"
-  version "upspin@aba3aa6-augie@5859abb"
-  sha256 "538304e6548eac1d27d1ca614182de827d3991c2059ad81ef490f84c74bf5d97"
+  version "upspin@1e73992-augie@b93eee0"
+  sha256 "fd9c07d9ac7d4e67176e569636b069cda52009cefb434e5f1953f774a2f3dc06"
 
   def install
     bin.install %w[
