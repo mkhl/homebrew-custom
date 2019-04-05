59bece909e9421ff0289e9a2ba4d5eb6f45ea783
Martin Kühl <martin.kuehl@gmail.com>
2019-04-05 22:13:30 +0200

    scdoc 1.9.4

--- Formula/scdoc.rb
+++ Formula/scdoc.rb
@@ -1,12 +1,12 @@
 class Scdoc < Formula
   desc "simple man page generator"
   homepage "https://git.sr.ht/~sircmpwn/scdoc"
-  url "https://git.sr.ht/~sircmpwn/scdoc/archive/1.8.0.tar.gz"
-  sha256 "24a5800062ffc8b21b91020a3289b20824cad4b42327957bed4cd815001bc984"
+  url "https://git.sr.ht/~sircmpwn/scdoc/archive/1.9.4.tar.gz"
+  sha256 "a10e200bcbc7d6faf9678444636886fee2fd99c754b630fc62fa9d247f1eec03"
   head "https://git.sr.ht/~sircmpwn/scdoc"
 
   def install
-    inreplace "Makefile", "install -Dm", "install -m"
+    # inreplace "Makefile", "install -Dm", "install -m"
     bin.mkpath
     man1.mkpath
     man5.mkpath
